1400da199b4feb39a58139544d37875f854f1808
Anas Nadeem <anas5678go@gmail.com>
2022-11-26 23:49:38 +0530

    GET and POST functionality added

--- js/task.js
+++ js/task.js
@@ -2,38 +2,33 @@ const BASE_API = "https://falakfatma.pythonanywhere.com/"
 const AUTH_URL = BASE_API + "api/"
 const TASK_URL_API = AUTH_URL + "todoslist"
 const token = localStorage.getItem('token')
-const headersData = {"Authorization": `Bearer ${token}`}
+const headersData = {"Authorization": `Bearer ${token}`, "Content-Type": "application/json"}
+let taskListId = document.getElementById('taskListId');
 
 // GET TASK
 const getTask = () => {
+
   const headersMethodGet = {method:'GET', headers: headersData}
   fetch(TASK_URL_API, headersMethodGet)
   .then((resp) => {
     return resp.json()
   })
   .then((data) => {
-    console.log(data)
+    data.forEach(task => {
+      taskListId.innerHTML += `<li class="list-group-item" id="task${task.id}">${task.name}</li>`;
+    });
   })
 }
 window.onload = getTask()
 
-// DELETE TASK
-const deleteTask = () => {
-  const headersMethodDelete = {method:'DELETE', headers: headersData}
-  fetch(TASK_URL_API, headersMethodDelete)
-  .then((resp) => {
-    console.log(resp)
-  })
-}
-
 // CREATE TASK 
 const createTask = (e) => {
     e.preventDefault();
-    let taskName = document.getElementById('taskName').value;
+    let taskName = document.getElementById('taskName');
     const todoslistData = {
-        'name': taskName
+        'name': taskName.value
     }
-    
+
     const header = {
         method: 'POST',
         headers: headersData,
@@ -42,18 +37,21 @@ const createTask = (e) => {
     fetch(TASK_URL_API, header)
     .then(response => {
         if (response.ok){
+          taskName.value = '';
             return response.json()
         }
         return Promise.reject(response);
     })
     .then(data => {
+        taskListId.innerHTML += `<li class="list-group-item" id="task${data.id}">${data.name}</li>`;
         return data;
     })
     .catch((errresp) => {
         errresp.json().then(err => {
             let errorMessageAlert = document.getElementById('errorMessageAlert');
             let errorMsg = document.getElementById('errorMsg');
-            errorMsg.innerHTML = err.error;
+            console.log('Error', err)
+            errorMsg.innerHTML = err;
             if (errorMessageAlert.style.display != 'block'){
                 errorMessageAlert.style.display = 'block';
             }
@@ -63,3 +61,12 @@ const createTask = (e) => {
 
 let taskFormId = document.getElementById('taskFormId');
 taskFormId.addEventListener('submit', createTask)
+
+// DELETE TASK
+const deleteTask = () => {
+  const headersMethodDelete = {method:'DELETE', headers: headersData}
+  fetch(TASK_URL_API, headersMethodDelete)
+  .then((resp) => {
+    console.log(resp)
+  })
+}
